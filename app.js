0d1ca93c0b0ad9003293926c21cb8a4fb9508117
gowtham-h <hosur.gowtham@gmail.com>
2016-04-21 00:28:31 +0530

    Story UI changes

--- app.js
+++ app.js
@@ -12,8 +12,8 @@ var mongoose = require('mongoose');
 
 var app = express();
 
-mongoose.connect('mongodb://172.23.238.253/frAgile_dummy')
-// mongoose.connect('mongodb://localhost/frAgile');
+//mongoose.connect('mongodb://172.23.238.253/frAgile_dummy')
+ mongoose.connect('mongodb://localhost/fragileDB');
 var db = mongoose.connection
 
 var routes = require('./routes/index');
