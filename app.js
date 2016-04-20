ef1d6ad94dbfcf3091d221f8b68412928c073039
unknown <shrinivaskamath143@gmail.com>
2016-04-20 18:01:59 +0530

    changed from local host to server db

--- app.js
+++ app.js
@@ -12,7 +12,8 @@ var mongoose = require('mongoose');
 
 var app = express();
 
-mongoose.connect('mongodb://localhost/fragileDB');
+mongoose.connect('mongodb://172.23.238.253/frAgile_dummy')
+// mongoose.connect('mongodb://localhost/frAgile');
 var db = mongoose.connection
 
 var routes = require('./routes/index');
