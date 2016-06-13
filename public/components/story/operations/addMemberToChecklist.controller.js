d8f650a7da8b73f45e17af8747f9dcad2581fc12
unknown <nitishonspeed@gmail.com>
2016-06-13 17:31:13 +0530

    Add member to item done

--- public/components/story/operations/addMemberToChecklist.controller.js
+++ public/components/story/operations/addMemberToChecklist.controller.js
@@ -7,7 +7,7 @@ $scope.currentItemId=param.listItem._id;
 
   $scope.initLoadMembersOfItem=function(){
     $scope.assignedMember=$scope.assignedMember.concat(param.listItem.assignedMember);
-    console.log("yahoooooooo,: ",$scope.assignedMember);
+    console.log("yahoooooooo,: ",param.listItem.assignedMember);
   }//end of load member Item
 
 	$scope.initLoadMembersOfStory = function(){
@@ -25,9 +25,8 @@ $scope.currentItemId=param.listItem._id;
   }//end of init load member
   
   $scope.addRemoveMembersList=function(memberObj){
-  
-      socket.emit('story:addRemoveMembersListItem',{"roomName":param.roomName,"memberObj":memberObj,"listObj":param.listItem});
-
+       // param.listItem.assignedMember.push(memberObj);
+      socket.emit('story:addRemoveMembersListItem',{"roomName":param.roomName,"memberObj":memberObj,"listItem":param.listItem});
 
           console.log("i have reached",param.listItem.assignedMember);
    
@@ -41,12 +40,17 @@ $scope.currentItemId=param.listItem._id;
 
    //
       socket.on('memberAdded',function(data){
-        console.log("event recieved");
-        if(data.listObj._id===$scope.currentItemId){
-          //console.log("adding in assigned member list");
+        console.log("event recieved --->",data.memberObj);
 
-         $scope.assignedMember.push(data.memberObj);
-         }
+        var check=$scope.assignedMember.filter(function(obj)
+        {
+            return obj._id==data.memberObj._id;
+        });
+        if(check.length==0)
+        {
+          $scope.assignedMember.push(data.memberObj);
+        }
+
        });
 
 	}]);//end of main app
